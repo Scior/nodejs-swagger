ddef6dab7bd49b81749ee2f52cb267180166d597
Scior <interval12@gmail.com>
2018-09-16 14:04:01 +0900

    [move] some files

--- src/router.js
+++ src/router.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 router.use('/', swaggerUi.serve);
 router.get('/', swaggerUi.setup(swaggerDocument));
-router.get('/api/v1/about', (req, res) => {
-    res.send('about');
+
+const apiBaseUrl = '/api/v1/';
+
+router.get(apiBaseUrl + 'about', (req, res) => {
+  res.send('about');
 });
 
-app.use('/', router);
-app.listen(3008, () => {
-    console.log('*_*');
-});
+module.exports = router;
